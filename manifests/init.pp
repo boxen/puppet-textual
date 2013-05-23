8379f083f0842f23093d15f047fd149cfb7f8467
Chris Boyle <chris@bbck.net>
2013-05-23 04:00:29 -0400

    Update to textual 3.0.2

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,8 @@
 #
 #   include textual
 class textual {
-  package { 'Textual':
-    source   => 'https://d3pep6299hwdsh.cloudfront.net/puppet-textual/Textual-3.0.1.zip',
+  package { 'Textual-3.0.2':
+    source   => 'https://d3pep6299hwdsh.cloudfront.net/puppet-textual/Textual-3.0.2.zip',
     provider => 'compressed_app'
   }
 }
--- spec/classes/textual_spec.rb
+++ spec/classes/textual_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'textual' do
   it do
-    should contain_package('Textual').with({
-      :source   => 'https://d3pep6299hwdsh.cloudfront.net/puppet-textual/Textual-3.0.1.zip',
+    should contain_package('Textual-3.0.2').with({
+      :source   => 'https://d3pep6299hwdsh.cloudfront.net/puppet-textual/Textual-3.0.2.zip',
       :provider => 'compressed_app',
     })
   end
