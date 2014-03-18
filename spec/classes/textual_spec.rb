e3569e8e83b1c47ecf54440df31b5852bff80f56
Chris Boyle <chris@bbck.net>
2014-03-18 17:06:32 -0400

    Update to Textual 4.1.3

--- spec/classes/textual_spec.rb
+++ spec/classes/textual_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'textual' do
   it do
-    should contain_package('Textual-4.1.1').with({
-      :source   => 'https://d3pep6299hwdsh.cloudfront.net/puppet-textual/Textual-4.1.1.zip',
+    should contain_package('Textual-4.1.3').with({
+      :source   => 'https://d3pep6299hwdsh.cloudfront.net/puppet-textual/Textual-4.1.3.zip',
       :provider => 'compressed_app',
     })
   end
