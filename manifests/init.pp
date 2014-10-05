41fe9557e7da26f2f719af5ff5e5e042e9b23c3d
Adam Ochonicki <adam@fromonesrc.com>
2014-10-05 14:04:53 -0400

    Fix broken module

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 #   include textual
 class textual {
-  package { 'Textual-4.1.5':
+  package { 'Textual':
     source   => 'https://d3pep6299hwdsh.cloudfront.net/puppet-textual/Textual-4.1.5.zip',
     provider => 'compressed_app'
   }
